f65365906050891cbd1a160550d022c45820b52f
sewphl <37678331+sewphl@users.noreply.github.com>
2022-11-07 09:47:55 -0500

    update style.js

--- js/style.js
+++ js/style.js
@@ -7,33 +7,44 @@ window.addEventListener("keydown", function (event) {
   if (event.key === "ArrowLeft"){
     changeSlide(-1);
   }
-  if (event.key === "Escape" && (document.getElementById("about").style.display==="block")) {
-    closeMe("about");
+
+  if (document.getElementById("about")) {
+    if (event.key === "Escape" && (document.getElementById("about").style.display==="block")) {
+      closeMe("about");
+    }
   }
   else if (event.key === "Escape" && (document.getElementById("Lightbox").style.display==="block")) {
     closeMe("Lightbox");
   }
-
 })
 
 var slideIndex = 1;
 showSlide(slideIndex);
 
 function openMe(id) {
-  //document.getElementById("gallery").style.display="none";
   let myModal = document.getElementById(id);
   if (myModal.style.display==="" || myModal.style.display==="none" ) {
-    let myPhotos = document.querySelectorAll(".photo");
-    let myFooter = document.getElementById("footer");
+    let myPhotos = document.querySelectorAll(".photo");  
+    let myHello = document.querySelector("#helloContent");
+    let myPrints = document.querySelector("#shopPrints");
+    let myHeader = document.querySelector("body > header-component");
+    let myFooter = document.querySelector("body > footer-component");
+
     for (let i = 0; i<myPhotos.length; i++) {
       myPhotos[i].style.opacity = "0";
     }
+    myHello.style.opacity = "0";
+    myPrints.style.opacity = "0";
+    myHeader.style.opacity = "0";
     myFooter.style.opacity = "0";
 
     setTimeout(function () {
       for (let i = 0; i<myPhotos.length; i++) {
         myPhotos[i].style.display = "none";
       }
+      myHello.style.display = "none";
+      myPrints.style.display = "none";
+      myHeader.style.display = "none";
       myFooter.style.display = "none";
     }, 1200);
 
@@ -55,8 +66,7 @@ function openMe(id) {
 
   myModal.style.display = "block";
   document.querySelector("body").style.overflow = "hidden"; //removes scroll
-  document.getElementById("uvVduid").style.cursor = "default";
-
+  //document.getElementById("uvVduid").style.cursor = "default"; 
 }
 
 function closeMe(id) {
@@ -70,13 +80,19 @@ function closeMe(id) {
     passive: false
   });
   document.querySelector("body").style.overflow = "visible"; //return to scroll
-  let myPhotos = document.querySelectorAll(".photo");
-  let myFooter = document.getElementById("footer");
+  let myPhotos = document.querySelectorAll(".photo");  
+  let myHello = document.querySelector("#helloContent");
+  let myPrints = document.querySelector("#shopPrints");
+  let myHeader = document.querySelector("body > header-component");
+  let myFooter = document.querySelector("body > footer-component");
 
   setTimeout(function () {
     for (let i = 0; i<myPhotos.length; i++) {
       myPhotos[i].style.opacity = "1";
     }
+    myHello.style.opacity = "1";
+    myPrints.style.opacity = "1";
+    myHeader.style.opacity = "1";
     myFooter.style.opacity = "1";
   }, 1600);
 
@@ -84,9 +100,11 @@ function closeMe(id) {
     for (let i = 0; i<myPhotos.length; i++) {
       myPhotos[i].style.display = "block";
     }
+    myHello.style.display = "block";
+    myPrints.style.display = "block";
+    myHeader.style.display = "block";
     myFooter.style.display = "block";
   }, 1200);
-  //document.getElementById("gallery").style.display="inline";
 }
 
 function changeSlide(n) {
